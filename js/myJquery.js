d70289ff2f59095420ffbb40b2ffe381c53fdc8b
Karen Johnson <kbthson@gmail.com>
2016-02-12 13:42:42 -0500

    fixed nav bar, added about us content, fixed other styling
    1.  fixed navbar.  Details in the file: myJquery.js.  Added id's and
    altered code to reference the parent of each, instead of the sibling.
    2. added text and styling to the about me page.
    3. removed the padding in the css section class, which was causing
    titles to be off center on some pages
    4. found the css error that was making the faq styling different: path
    to the css files was wrong.

--- js/myJquery.js
+++ js/myJquery.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 	$.get("includes/header.inc.html", function(data) {
 		$("#header").html(data);
+		var pageId = $("body").attr("id");
+		var navId = "#" + pageId + "-nav-link";
+		$(navId).parent().addClass("active");
 	});
 	
 	$.get("includes/footer.inc.html", function(data) {
 		$("#footer").html(data);
 	});
 
-
-	$("li a").click (function (e) {
-		$(this).addClass('active').siblings().removeClass('active');
-	});
 
 });
 
