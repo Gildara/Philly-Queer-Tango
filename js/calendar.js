7e0d54c0bc951246bc7069853d83b02e177c244c
Gildara <maryann573@gmail.com>
2016-02-24 16:14:26 -0500

    calendar fix

--- js/calendar.js
+++ js/calendar.js
@@ -22,7 +22,22 @@ $('#calendar').fullCalendar({
 					}
 					calendar.fullCalendar('unselect');
 				},
-				editable: true		
-    })
-			
-});
+				editable: true,
+				weekMode: 'liquid',
+				url:'#',
+				events: [
+					{
+						title: 'Tango Hop',
+						start: new Date(2016, 1, 9, 6),
+						end: new Date(2016, 1, 9, 10),
+						allDay: false
+					},
+					{
+						title: 'Tango Meetup',
+						start: new Date(2016, 1, 11, 00),
+                    allDay: false
+                }
+            ]
+        });
+         
+    });
